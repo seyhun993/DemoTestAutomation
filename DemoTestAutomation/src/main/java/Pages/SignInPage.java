9a6fd5608df5ae88f4e71207b984c1af94db75a8
seyhun993 <seyhun993@gmail.com>
2024-02-20 18:22:13 +0300

    fix

--- DemoTestAutomation/src/main/java/Pages/SignInPage.java
+++ DemoTestAutomation/src/main/java/Pages/SignInPage.java
@@ -1,5 +1,6 @@
 package Pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,34 +11,72 @@ public class SignInPage {
 
     public static WebDriver webDriver;
 
-    public SignInPage(WebDriver driver){
-        this.webDriver=driver;
+    public SignInPage(WebDriver driver) {
+        this.webDriver = driver;
 
-        }
+    }
 
     public static void ProductSearch(String text) {
         WebElement search = BaseTest.webDriver.findElement(By.id("twotabsearchtextbox"));
         search.sendKeys(text);
     }
-  public static void clickButton(){
-       WebElement aramaButon = webDriver.findElement(By.id("nav-search-submit-button"));
-      aramaButon.click();
+
+    public static void clickButton() {
+        WebElement aramaButon = webDriver.findElement(By.id("nav-search-submit-button"));
+        aramaButon.click();
 
     }
 
-   public static void cerezButon(){
+    public static void cerezButon() {
         WebElement cerezKabulEt = webDriver.findElement(By.id("sp-cc-accept"));
 
-        if (cerezKabulEt.isDisplayed()==true){
+        if (cerezKabulEt.isDisplayed() == true) {
             cerezKabulEt.click();
-
         }
-
     }
 
+
+    public static void clickOnProductDescription() {
+        WebElement clickProduct = webDriver.findElement(By.cssSelector("div._bXVsd_gridColumn_2Jfab:nth-child(1) > div:nth-child(1) > div:nth-child(3) > a:nth-child(1) > span:nth-child(1) > span:nth-child(2)"));
+        clickProduct.click();
+
+    }
+    public static SignInPage checkProductDescriptionText(String actualText){
+    String expectedText= webDriver.findElement(By.id("productTitle")).getText();
+    Assert.assertEquals(actualText, expectedText);
+    System.out.println(expectedText);
+    return null;
+    }
+
+    public static void addToCart() {
+        WebElement addToCartButton = webDriver.findElement(By.cssSelector("#add-to-cart-button"));
+        addToCartButton.click();
+    }
+
+    public static SignInPage checkProductAddedTex(String actualText) {
+        String expectedText = webDriver.findElement(By.cssSelector(".a-size-medium-plus")).getText();
+        Assert.assertEquals(actualText, expectedText);
+        System.out.println(expectedText);
+        return null;
+    }
+
+    public static void goToCart() {
+        WebElement goToCartButton = webDriver.findElement(By.cssSelector("#sw-gtc > span:nth-child(1) > a:nth-child(1)"));
+        goToCartButton.click();
+
+    }
+    public static SignInPage checkAlisverisSepeti(String actualText) {
+        String expectedText = webDriver.findElement(By.cssSelector(".sc-cart-header > div:nth-child(1) > h1:nth-child(1)")).getText();
+        Assert.assertEquals(actualText, expectedText);
+        System.out.println(expectedText);
+        return null;
+    }
 
 
 }
+
+
+
 
 
 
--- DemoTestAutomation/src/main/java/Test/SignInTest.java
+++ DemoTestAutomation/src/main/java/Test/SignInTest.java
@@ -7,23 +7,51 @@ import org.testng.annotations.Test;
 
 public class SignInTest extends BaseTest {
 
-  @Test
-    public void TC001(){
+    @Test
+    public void TC001() {
 
         SignInPage signInPage = new SignInPage(webDriver);
         SignInPage.cerezButon();
-        SignInPage.ProductSearch("telefon");
+        SignInPage.ProductSearch("Samsung Galaxy S23 Ultra 256 GB | 8 GB RAM Cep Telefonu, Green (Samsung Türkiye Garantili");
         SignInPage.clickButton();
+        SignInPage.clickOnProductDescription();
+        SignInPage.checkProductDescriptionText("Samsung Galaxy S23 Ultra 256 GB | 8 GB RAM Cep Telefonu, Green (Samsung Türkiye Garantili) ");
+        SignInPage.addToCart();
+        SignInPage.checkProductAddedTex("Sepete Eklendi");
+        SignInPage.goToCart();
 
+    }
 
+    @Test
+    public void TC002() {
 
+        SignInPage signInPage = new SignInPage(webDriver);
+        SignInPage.cerezButon();
+        SignInPage.ProductSearch("Samsung Galaxy S23 Ultra 256 GB | 8 GB RAM Cep Telefonu, Green (Samsung Türkiye Garantili");
+        SignInPage.clickButton();
+        SignInPage.clickOnProductDescription();
+        SignInPage.addToCart();
+        SignInPage.checkProductAddedTex("Sepete Eklendi");
+    }
+    @Test
+    public void TC003() {
 
-
-
-
+        SignInPage signInPage = new SignInPage(webDriver);
+        SignInPage.cerezButon();
+        SignInPage.ProductSearch("Samsung Galaxy S23 Ultra 256 GB | 8 GB RAM Cep Telefonu, Green (Samsung Türkiye Garantili");
+        SignInPage.clickButton();
+        SignInPage.clickOnProductDescription();
+        SignInPage.addToCart();
+        SignInPage.goToCart();
+        SignInPage.checkAlisverisSepeti("Alışveriş Sepeti");
 
 
     }
 
 
 }
+
+
+
+
+
